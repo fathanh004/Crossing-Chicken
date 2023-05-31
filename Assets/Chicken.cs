7f305ac2bb6dbda6c8d9d547b5808e70be8e8691
fathanh004 <fathanhidayatullah.2020@student.uny.ac.id>
2023-05-31 22:43:04 +0700

    add swipe input

--- Assets/Chicken.cs
+++ Assets/Chicken.cs
@@ -21,21 +21,44 @@ public class Chicken : MonoBehaviour
     public UnityEvent OnDie;
     public UnityEvent OnMovingToBackLimit;
 
+    //for mobile input
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+
+
     float timer;
     // Update is called once per frame  
 
-    private void Start() {
+    private void Start()
+    {
         AudioListener.volume = 1;
     }
-    
+
     void Update()
     {
         if (isUnmoveable)
         {
             return;
         }
+
+        //for mobile input
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTouchPosition = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                endTouchPosition = touch.position;
+                HandleSwipe();
+            }
+        }
+
         //if no input in 5 seconds, do idle animation
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown || Input.touchCount > 0)
         {
             animator.SetBool("Turn Head", false);
             timer = 0;
@@ -177,5 +200,41 @@ public class Chicken : MonoBehaviour
     public void SetUnmoveable(bool value)
     {
         isUnmoveable = value;
+    }
+
+    void HandleSwipe()
+    {
+        Vector2 swipeDirection = endTouchPosition - startTouchPosition;
+
+        if (swipeDirection.magnitude < 20)
+        {
+            return; // Ignore small swipes
+        }
+
+        swipeDirection.Normalize();
+
+        Vector3 direction = Vector3.zero;
+
+        if (swipeDirection.y > 0.5f)
+        {
+            direction += Vector3.forward;
+        }
+        else if (swipeDirection.y < -0.5f)
+        {
+            direction += Vector3.back;
+        }
+        else if (swipeDirection.x < -0.5f)
+        {
+            direction += Vector3.left;
+        }
+        else if (swipeDirection.x > 0.5f)
+        {
+            direction += Vector3.right;
+        }
+
+        if (direction != Vector3.zero)
+        {
+            Move(direction);
+        }
     }
 }
